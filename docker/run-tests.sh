44335d14d7f69b4c321e75451465fad108d81e44
nessi-dev <nessi.datatool@gmail.com>
2025-05-05 00:01:28 +0200

    update docs and tests

--- docker/run-tests.sh
+++ docker/run-tests.sh
@@ -8,9 +8,9 @@ docker-compose -f docker-compose.test.yml down -v
 docker-compose -f docker-compose.test.yml build
 docker-compose -f docker-compose.test.yml up --abort-on-container-exit
 
-# Copy test results to host
+# Copy test results from volume to host
 mkdir -p ../test-results
-docker cp $(docker-compose -f docker-compose.test.yml ps -q test-runner):/app/test-results/ ../test-results/
+docker run --rm -v nessi_test-results:/app/test-results -v $(pwd)/../test-results:/host alpine sh -c "cp -r /app/test-results/* /host/"
 
 # Clean up
 docker-compose -f docker-compose.test.yml down -v 
